3a16e9df7f74a5d99a6b6b70b66e0a86077f7459
csirick2020 <144964085+csirick2020@users.noreply.github.com>
2025-04-05 00:48:23 +0000

    Added Tech with Tim tutorial

---
+++ tech_tim_website.py
@@ -0,0 +1,18 @@
+# Tech with Time YouTube URL: https://youtu.be/o8p7uQCGD0U?si=IMFm2bjI1tH-8Ti1
+
+# Use 'streamlit run APP_NAME.py' in terminal to run the application
+
+import streamlit as st
+
+# st.write is considered a "magic command" in streamlit because
+# it will automatically write pretty much anything you give it.
+# Regular text (string)
+st.write("hello world 123")
+# Python object, like key, value pair
+st.write({"key": "value"})
+# Boolean value
+st.write(True)
+# Numbers (no string quotes)
+st.write(123456)
+
+# TODO: Will have to finish later as too much is going on right now...
