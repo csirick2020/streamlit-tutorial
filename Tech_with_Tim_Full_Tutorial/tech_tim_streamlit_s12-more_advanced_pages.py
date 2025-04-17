59f4cef9620cb3f14327cc7044fbb20797465344
csirick2020 <144964085+csirick2020@users.noreply.github.com>
2025-04-17 02:47:04 +0000

    Added/completed this file

---
+++ Tech_with_Tim_Full_Tutorial/tech_tim_streamlit_s12-more_advanced_pages.py
@@ -0,0 +1,4 @@
+# Psych! This is just a link which Tim referred to at the end of the video
+# while skimming over the documentation contained therein...
+
+# Link: https://docs.streamlit.io/develop/concepts/multipage-apps/pages-directory
