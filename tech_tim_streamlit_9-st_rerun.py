58ab8dc142db1b84adaa4b908ce2cc7b721b4eeb
csirick2020 <144964085+csirick2020@users.noreply.github.com>
2025-04-17 00:13:07 +0000

    Completed this file

--- tech_tim_streamlit_9-st_rerun.py
+++ tech_tim_streamlit_9-st_rerun.py
@@ -2,4 +2,26 @@
 
 # Use 'streamlit run APP_NAME.py' in terminal to run the application
 
+# Forces the app to re-run itself,
+# which can help solve problems we
+# saw in previous examples.
+
 import streamlit as st
+
+st.title("Counter Example with Immediate Rerun")
+
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+def increment_and_rerun():
+    st.session_state.count += 1
+
+    # Look at the line below...
+    # If it were commented out, our counter
+    # would always be lagging behind by one.
+    st.rerun()  # Manually invoke a rerun to update the state for us
+
+st.write(f"Current Count: {st.session_state.count}")
+
+if st.button("Increment and Update Immediately"):
+    increment_and_rerun()
